d4d1e007f93799c8fb2b57b451f3881e6f2e0a63
Ben Steele <BS@Bens-MacBook-Air.local>
2015-05-11 15:42:45 -0600

    multiple patterns

--- RabinKarp.java
+++ RabinKarp.java
@@ -12,15 +12,41 @@ public class RabinKarp {
     public RabinKarp(String _text) {
         text = _text;
     }
+    
     //find multiple patterns at the same time
     public void findPatterns(String[] patterns) {
-        for (String p : patterns) {
-            targets.put(p,false);
+        ts = patterns;
+        targets = new HashMap<String, Boolean>();
+        int size = 1000000;
+        int checkable = 0;
+        ArrayList<Integer> sizes = new ArrayList<Integer>();
+        //put the pattern string in the hash table so it can be compared
+        for(String s : patterns) {
+            //put the target string in the hash table
+            targets.put(s,false);
+            //find the shortest string length
+            if(!sizes.contains(s.length())) {
+                sizes.add(s.length());
+            }
+            if(s.length() < size) {
+                size = s.length();
+                checkable = text.length() - size;
+            }
         }
-        for(int i=0;i<2;i++) {
-
+        //iterate through every index in the text, leaving out indices leaving less than the size of the target at the end
+        for(int i=0;i<checkable;i++) {
+            //get strings for the size of each input at the current index
+            //see if those strings are in the hash table
+            for(int s : sizes) {
+                if(i+s < text.length()) {    //dont check strings that will go past the end of the text indices
+                    if (targets.containsKey(text.substring(i,i+s))) {
+                        targets.put(text.substring(i,i+s),true);    //if the current substring is in the hash table, mark the that hash as true 
+                    }
+                }
+            }
         }
     }
+    
     //find a single pattern
     public int findPattern(String pattern) {
         //initiate ts
@@ -54,7 +80,11 @@ public class RabinKarp {
     
     public static void main(String[] args) {
         RabinKarp m = new RabinKarp("This is my test string");
-        m.findPattern("is");
+        String[] p = new String[3];
+        p[0] = "is";
+        p[1] = "his";
+        p[2] = "tst";
+        m.findPatterns(p);
         m.printResults();
     }
 }
